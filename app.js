a1cdb046c392e27363bab155402b25ab3438ce85
Jose Cáceres <joseluis8906@opmbx.org>
2018-02-01 20:55:35 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express ();
-
-app.get("/", (req, res) => res.send('hello tama'));
-app.get("/jose", (req, res) => res.send('hello jose'));
-app.get("/joiner", (req, res) => res.send('hello joiner'));
-
-app.listen(3000, () => console.log("listen port: 3000"));
