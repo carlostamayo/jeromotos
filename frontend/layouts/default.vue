fa78a3f8227c270aa5f2d431b4b075ef30bcef24
joseluis8906 <joseluis8906@opmbx.org>
2018-02-11 18:22:01 -0500

    actualizado

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,34 +1,42 @@
-<template lang="pug">
-doctype html
-v-app(id="sandbox" :dark="dark" :light="!dark" standalone)
-  v-navigation-drawer(v-model="primaryDrawer.model" persistent='persistent' dark)
-    v-list(dense)
-      template(v-for="(item, i) in items")
-        v-list-tile(:key="i" nuxt link :to="item.to")
-          v-list-tile-action
-            v-icon {{ item.icon }}
-
-          v-list-tile-content
-            v-list-tile-title {{ item.text }}
-
-      v-list-tile(@click.native="logout")
-        v-list-tile-action
-          v-icon exit_to_app
-
-        v-list-tile-content
-          v-list-tile-title Salir
-
-  v-toolbar(fixed)
-    v-toolbar-side-icon(@click.native.stop="primaryDrawer.model = !primaryDrawer.model" v-if="primaryDrawer.type !== 'permanent'")
-    v-toolbar-title {{ title }}
-
-  main
-    v-container(fluid mb-5 pt-0)
-      nuxt
-
-  v-footer(fixed)
+<template>
+<!DOCTYPE html>
+<v-app id="sandbox" :dark="dark" :light="!dark" standalone>
+  <v-navigation-drawer v-model="primaryDrawer.model" persistent='persistent' dark>
+    <v-list dense>
+      <template v-for="(item, i) in items">
+        <v-list-tile :key="i" nuxt link :to="item.to">
+          <v-list-tile-action>
+            <v-icon> {{ item.icon }} </v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title> {{ item.text }} </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+      <v-list-tile @click.native="logout">
+        <v-list-tile-action>
+          <v-icon> exit_to_app </v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title> Salir </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+  <v-toolbar fixed>
+    <v-toolbar-side-icon @click.native.stop="primaryDrawer.model = !primaryDrawer.model" v-if="primaryDrawer.type !== 'permanent'">
+      <v-toolbar-title> {{ title }} </v-toolbar-title>
+    </v-toolbar-side-icon>
+  </v-toolbar>
+  <main>
+    <v-container fluid mb-5 pt-0>
+      <nuxt/>
+    </v-container>
+  </main>
+  <v-footer fixed>
     <span> © {{ new Date().getFullYear() }} </span> Jose Luis Cáceres Escudero
-
+  </v-footer>
+</v-app>
 </template>
 
 <script>
